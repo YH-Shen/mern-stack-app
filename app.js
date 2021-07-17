2f3389974f25a09fb022597b025cdefebd073e20
Yuhui Shen <yuhui.shen@hotmail.com>
2021-07-16 23:58:16 -0400

    initialize server and react app

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+app.use(cookieParser());
+// body pareser in express
+app.use(express.json());
+
+
+// connect mongodb locally
+// pass in the useNewUrlParser option to avoid future deprecation warning 
+mongoose.connect("mongodb://localhost:27017/mernauth", {useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log("Successfully connected to database!");
+});
+
+// listen port 5000 and avoid react app's 3000 port
+app.listen(5000, ()=>{
+    console.log("express server started")
+})
