f147d40756ccc8d2df8e9785fc835feb62d2dd47
Yuhui Shen <yuhui.shen@hotmail.com>
2021-07-17 17:15:45 -0400

    post and get todos route works

--- routes/User.js
+++ routes/User.js
@@ -58,4 +58,33 @@ userRouter.get("/logout", passport.authenticate("jwt", {session: false}), (req, 
     res.json({user: {username: "", role: ""}, success: true});
 });
 
+userRouter.post("/todo", passport.authenticate("jwt", {session: false}), (req, res)=>{
+    const todo = new Todo(req.body);
+    todo.save((err) => {
+        if (err) {
+            res.status(500).json({message: {msgBody: "Error has occured", msgError: true}});
+        }else {
+            req.user.todos.push(todo);
+            req.user.save((err) => {
+                if (err) {
+                    res.status(500).json({message: {msgBody: "Error has occured", msgError: true}});
+                } else {
+                    res.status(200).json({message: {msgBody: "Successfully created todo", msgError: false}});
+                }
+            })
+        }
+
+    })
+});
+
+userRouter.get("/todos", passport.authenticate("jwt", {session: false}), (req, res)=>{
+    User.findById({_id: req.user._id}).populate("todos").exec((err, document) => {
+        if (err) {
+            res.status(500).json({message: {msgBody: "Error has occured", msgError: true}});
+        } else {
+            res.status(200).json({todos: document.todos, authenticated: true});
+        }
+    })
+});
+
 module.exports = userRouter;
