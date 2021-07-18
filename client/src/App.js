37a36de2ea7531b4988407916bb3a3940e7359f3
Yuhui Shen <yuhui.shen@hotmail.com>
2021-07-18 19:10:25 -0400

    register page done

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import Login from './Components/Login';
+import Register from './Components/Register';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
@@ -13,6 +14,8 @@ function App() {
       <Navbar />
       <Route exact path="/" component={Home}/>
       <Route path="/login" component={Login}/>
+      <Route path="/register" component={Register}/>
+
 
     </Router>
   );
