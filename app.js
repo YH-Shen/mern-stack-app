59367802a48ccae9bb09c568e44926381ef4982b
Yuhui Shen <yuhui.shen@hotmail.com>
2021-07-21 13:00:20 -0400

    re-wrote app

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/mernauth',{useNewUrlParser : true,useUnifiedTopology: true},()=>{
+    console.log('successfully connected to database');
+});
+
+const userRouter = require('./routes/User');
+app.use('/user',userRouter);
+
+app.listen(5000,()=>{
+    console.log('express server started');
+});
